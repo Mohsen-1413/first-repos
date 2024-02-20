6750805fd57f6a6b348717726ef31de70b10a1dc
Mohsen <mohsen.shsfaei@yahoo.com>
2024-02-20 14:43:17 +0300

    version 6

--- ddd.js
+++ ddd.js
@@ -4,4 +4,4 @@ style[1]= "Classi";
 style.shift(0);
 style.unshift("hello","bye");
 console.log("fjhvbjfnvdjf bceuicmekjcnw");
-console.log(" I must do it now");
+console.log(" I must do it now hhhhhhh");
